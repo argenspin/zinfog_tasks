c4bec7dc251a45cd058200bbc516f4f70fa8937e
Rizwaan Salim <argenspin@gmail.com>
2024-03-05 19:57:53 +0530

    Added initial files

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Zinfog Tasks",
+    'author': 'Rizwaan',
+    'version': "15.0.1.0",
+    'depends': ['base','stock','sale_management','account'],
+    'data': [
+
+    ],
+    'demo': [],
+    'summary': "Zinfog Tasks by Rizwaan",
+    'description': "",
+    'installable': True,
+    'auto_install': False,
+    'license': "LGPL-3",
+    'application': False
+}
